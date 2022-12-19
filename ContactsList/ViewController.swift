ede1b8dd344664d2f80a9239a0774ad04db3257e
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-19 13:59:20 +0300

    Add project files

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Pavel on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
